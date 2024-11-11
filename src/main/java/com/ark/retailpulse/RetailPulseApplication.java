0c9e726001900918b6b1db8a86d1943e0ad820aa
anirudhrk01 <anirudhrk01@gmail.com>
2024-11-11 11:42:53 +0530

    base of project

---
+++ src/main/java/com/ark/retailpulse/RetailPulseApplication.java
@@ -0,0 +1,13 @@
+package com.ark.retailpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailPulseApplication.class, args);
+    }
+
+}
