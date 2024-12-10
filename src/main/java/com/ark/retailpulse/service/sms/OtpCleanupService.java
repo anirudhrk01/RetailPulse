cb6654f0fa7d81e2ffc00f6c9538f93da02016d2
anirudhrk01 <anirudhrk01@gmail.com>
2024-12-10 16:48:14 +0530

    [chores]: otp cleanup service

--- src/main/java/com/ark/retailpulse/service/sms/OtpCleanupService.java
+++ src/main/java/com/ark/retailpulse/service/sms/OtpCleanupService.java
@@ -14,11 +14,18 @@ public class OtpCleanupService {
 
     private final OtpRepository otpRepository;
 
+    /**
+     * Cleans up expired OTPs (both SMS and email) every 1 minute.
+     */
     @Transactional
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 60000) // Runs every 1 minute (60000 milliseconds)
     public void cleanupExpiredOtps() {
         LocalDateTime now = LocalDateTime.now();
+
+        // Delete expired SMS OTPs
         otpRepository.deleteAllBySmsOtpExpirationTimeBefore(now);
+
+        // Delete expired email OTPs
         otpRepository.deleteAllByEmailOtpExpirationTimeBefore(now);
     }
 }
