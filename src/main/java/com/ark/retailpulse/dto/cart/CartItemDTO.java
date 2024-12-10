7a3612fbb1cf5c25232c4af1d64b368cf77aaf28
anirudhrk01 <anirudhrk01@gmail.com>
2024-12-10 19:08:54 +0530

    [chores]: added javadoc for dtos

--- src/main/java/com/ark/retailpulse/dto/cart/CartItemDTO.java
+++ src/main/java/com/ark/retailpulse/dto/cart/CartItemDTO.java
@@ -1,18 +1,17 @@
 package com.ark.retailpulse.dto.cart;
 
 import com.ark.retailpulse.dto.product.ProductDTO;
-import com.ark.retailpulse.model.Cart;
-import com.ark.retailpulse.model.Product;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
+/**
+ * DTO representing an item in the shopping cart.
+ */
 @Data
 public class CartItemDTO {
 
     private Long id;
 
-    private CartDTO cart; //to be edited
+    private CartDTO cart;
 
     private ProductDTO product;
 
