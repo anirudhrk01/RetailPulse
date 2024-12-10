b84959ed9a7f0edede453c8ffdb971164e322825
anirudhrk01 <anirudhrk01@gmail.com>
2024-12-10 17:45:33 +0530

    [chores ] clean repositories

--- src/main/java/com/ark/retailpulse/repository/CartRepository.java
+++ src/main/java/com/ark/retailpulse/repository/CartRepository.java
@@ -6,19 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-/**
- * Repository interface for accessing Cart entities from the database.
- * Extends JpaRepository to provide CRUD operations and custom queries for Cart entities.
- */
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    /**
-     * Finds a Cart by the user's ID.
-     * This method is used to retrieve a Cart for a specific user.
-     *
-     * @param userId the ID of the user
-     * @return an Optional containing the Cart if found, otherwise empty
-     */
     Optional<Cart> findByUserId(Long userId);
 }
--- src/main/java/com/ark/retailpulse/repository/UserRepository.java
+++ src/main/java/com/ark/retailpulse/repository/UserRepository.java
@@ -4,14 +4,12 @@ import com.ark.retailpulse.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByPhoneNumber(String phoneNumber);
-//    void deleteByOtpVerifiedFalseAndCreatedAtBefore(LocalDateTime timestamp);
 
 
     boolean existsByEmail(String email);
